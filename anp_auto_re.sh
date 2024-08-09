261fac09ea4c8037a1a0aa9f6b82eab4adca1bf1
Lingjie <13974726267@163.com>
2024-08-09 22:18:29 +0800

    Update anp_auto_re.sh

--- anp_auto_re.sh
+++ anp_auto_re.sh
@@ -8,6 +8,7 @@ DATA_DIR=$(realpath ${1})
 ROOT_DIR=$(dirname ${DATA_DIR})
 REG_DIR=${ROOT_DIR}/anp_reged_data_$(date +"%m%d%H%M%S")
 WORK_SCRIPT=/home/caros/cybertron/scripts/offline_x86_test.sh
+total_case=0
 
 # --- func color ---
 function green(){
@@ -62,18 +63,18 @@ function decode() {
 	source /home/caros/venv/bin/activate >/dev/null 2>&1
 	cd /home/caros/cybertron_parse-master && source ./cybertron/setup.bash
 	for record_name in $(ls ${REG_DIR} | grep "\.record"); do
+ 		((total_case++))
 		green "Start Decoding Record: ${record_name} ..."
 		input_path=${REG_DIR}/${record_name}
 		output_path=${REG_DIR}/${record_name/\.record/''} && mkdir -p ${output_path}
 		python decode_record/decode_record_mp.py ${input_path} ${output_path} 2>/dev/null
-		green "${record_name} Decode Finish !"
+		green "No.${total_case} ${record_name} Decode Finish !"
 	done
 }
 
 function evaluate() {
 	eval_tools_dir=/home/caros/whole_functional_test_evaluation_tool
 	sed -i "/root_dir/{s/: .*/: \"${ROOT_DIR//\//\\\/}\/\"/}" ${eval_tools_dir}/configs/cfg_tool.yaml
-	total_case=0
 	for case in $(ls ${REG_DIR} | grep -v "\.record"); do
 		# set /gt dir & /res_record dir
 		case_gt_dir=${ROOT_DIR}/gt/${case} && mkdir -p ${case_gt_dir}
@@ -88,7 +89,6 @@ function evaluate() {
 		cp ${json_file} ${case_js_dir}
 		# total info
 		module=$(cat ${yaml_file} | grep module | awk '{print $2}')
-		((total_case++))
 	done
 	cd ${eval_tools_dir} && sudo python3 ./run_evaluation.py
 }
